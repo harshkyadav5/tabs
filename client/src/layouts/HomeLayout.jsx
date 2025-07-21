ed52cfe5df75daa159d011ae0d2c120259b8f991
harshkyadav5 <harshkyadav5@gmail.com>
2025-07-22 00:39:31 +0530

    Fix the position of content for screens with side menu

--- client/src/layouts/HomeLayout.jsx
+++ client/src/layouts/HomeLayout.jsx
@@ -4,9 +4,9 @@ import SideMenu from "../components/SideMenu";
 
 export default function HomeLayout() {
   return (
-    <div className="flex">
+    <div className="flex h-screen w-full overflow-hidden">
       <SideMenu />
-      <main className="ml-80 py-20 px-8 max-w-4xl w-full">
+      <main className="flex-1 pt-24 pb-16 pr-10 pl-5 overflow-y-auto">
         <Outlet />
       </main>
     </div>

59a1f71da545989f084bd305fc8a95bc34d737c6
harshkyadav5 <harshkyadav5@gmail.com>
2025-07-26 09:38:48 +0530

    Add AuthContext to main.jsx and add logout button and username in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
+import { useAuth } from "../context/AuthContext";
 
 export default function Navbar() {
+  const { user, logout } = useAuth();
+
   return (
     <>
       <div
@@ -41,41 +44,61 @@ export default function Navbar() {
         </div>
 
         <div className="flex items-center">
-          <div className="flex items-center space-x-4">
-            <Link to="/signin">
+          {user ? (
+            <>
+              <span className="text-sm mr-2 font-medium text-gray-700 dark:text-gray-200">
+                {user.username}
+              </span>
               <button
+                onClick={logout}
                 className="
                   font-montserrat text-sm font-semibold
                   px-4 py-2
-                  bg-gray-900 text-white
-                  border border-black dark:border-gray-100
-                  dark:bg-gray-100 dark:text-black
+                  bg-red-600 text-white
                   rounded-lg shadow-sm
-                  hover:bg-gray-700 dark:hover:bg-gray-300
+                  hover:bg-red-700
                   transition-colors duration-200
                 "
               >
-                Sign In
+                Logout
               </button>
-            </Link>
-          </div>
-
-          <Link to="/signup">
-            <button
-              className="
-                ml-2
-                font-montserrat text-sm font-semibold
-                px-4 py-2
-                border border-gray-900 text-gray-900
-                dark:border-gray-100 dark:text-gray-100
-                rounded-lg
-                hover:bg-gray-200 dark:hover:bg-gray-800
-                transition-colors duration-200
-              "
-            >
-              Sign Up
-            </button>
-          </Link>
+            </>
+          ) : (
+            <>
+              <Link to="/signin">
+                <button
+                  className="
+                    font-montserrat text-sm font-semibold
+                    px-4 py-2
+                    bg-gray-900 text-white
+                    border border-black dark:border-gray-100
+                    dark:bg-gray-100 dark:text-black
+                    rounded-lg shadow-sm
+                    hover:bg-gray-700 dark:hover:bg-gray-300
+                    transition-colors duration-200
+                  "
+                >
+                  Sign In
+                </button>
+              </Link>
+              <Link to="/signup">
+                <button
+                  className="
+                    ml-2
+                    font-montserrat text-sm font-semibold
+                    px-4 py-2
+                    border border-gray-900 text-gray-900
+                    dark:border-gray-100 dark:text-gray-100
+                    rounded-lg
+                    hover:bg-gray-200 dark:hover:bg-gray-800
+                    transition-colors duration-200
+                  "
+                >
+                  Sign Up
+                </button>
+              </Link>
+            </>
+          )}
 
           <div className="h-6 w-px bg-gray-300 dark:bg-gray-600 mx-3" />
 

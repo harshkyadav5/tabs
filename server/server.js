377c03ad590851e055554be818b3e904c4fdf839
harshkyadav5 <harshkyadav5@gmail.com>
2025-08-12 23:56:51 +0530

    Add the support in server for music search based on album, playlist, track, artist

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from "./routes/authRoutes.js";
 import clipboardRoutes from "./routes/clipboardRoutes.js";
+import musicRoutes from "./routes/musicRoutes.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/music", musicRoutes);
 app.use("/api/clipboard", clipboardRoutes);
 
 app.listen(PORT, () => {
